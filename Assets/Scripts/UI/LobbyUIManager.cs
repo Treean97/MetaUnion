051d99f9f739bf8a0837ab805ef80aa0dab795eb
Weminsoo <weminsoo@naver.com>
2025-06-29 17:36:07 +0900

    로비 새로고침 기능 추가

--- Assets/Scripts/UI/LobbyUIManager.cs
+++ Assets/Scripts/UI/LobbyUIManager.cs
@@ -1,77 +1,92 @@
 using UnityEngine;
+using Photon.Pun;               
 using Photon.Realtime;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class LobbyUIManager : MonoBehaviour
+public class LobbyUIManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private Transform _RoomListContent;           // ScrollView Content
-    [SerializeField] private GameObject _RoomItemPrefab;
-    [SerializeField] private Button _JoinRoomButton;       // 입장 버튼
-    [SerializeField] private Button _CreateRoomButton;
+    [SerializeField] private Transform _RoomListContent;
+    [SerializeField] private GameObject  _RoomItemPrefab;
+    [SerializeField] private Button      _JoinRoomBtn;
+    [SerializeField] private Button      _CreateRoomBtn;
+    [SerializeField] private Button      _RefreshBtn;
 
     private RoomInfo _SelectedRoomInfo;
 
-    private void OnEnable()
+    // OnEnable 오버라이드
+    public override void OnEnable()
     {
-        // 로비 열림 이벤트 호출
+        base.OnEnable();  // ← Photon 콜백 등록
+
         GameEvents.RaiseOpenLobbyUI();
-
-        // 기존 코드 유지
-        GameEvents.OnSelectRoom += HandleSelectRoom;
-
-        // ✅ 방 목록 수신 이벤트 구독
+        GameEvents.OnSelectRoom      += HandleSelectRoom;
         GameEvents.OnRoomListUpdated += HandleUpdateRoomList;
 
-        // 최신 방 목록 수동 초기화 (캐시 사용)
         var currentRoomList = CachedRoomList.GetRoomList();
         if (currentRoomList != null)
-        {
             HandleUpdateRoomList(currentRoomList);
-        }       
 
-        _JoinRoomButton.interactable = false;
+        _JoinRoomBtn.interactable = false;
         _SelectedRoomInfo = null;
 
-        _JoinRoomButton.onClick.RemoveAllListeners();
-        _JoinRoomButton.onClick.AddListener(OnJoinRoomButtonClicked);
-        _CreateRoomButton.onClick.AddListener(OnCreateRoomButtonClicked);
+        _JoinRoomBtn.onClick.RemoveAllListeners();
+        _JoinRoomBtn.onClick.AddListener(OnJoinRoomButtonClicked);
+
+        _CreateRoomBtn.onClick.RemoveAllListeners();
+        _CreateRoomBtn.onClick.AddListener(OnCreateRoomButtonClicked);
+
+        _RefreshBtn.onClick.RemoveAllListeners();
+        _RefreshBtn.onClick.AddListener(OnRefreshButtonClicked);
     }
 
-    private void OnDisable()
+    // OnDisable 오버라이드
+    public override void OnDisable()
     {
-        GameEvents.OnSelectRoom -= HandleSelectRoom;
+        base.OnDisable(); // ← Photon 콜백 해제
+
+        GameEvents.OnSelectRoom      -= HandleSelectRoom;
         GameEvents.OnRoomListUpdated -= HandleUpdateRoomList;
+
+        _RefreshBtn.onClick.RemoveListener(OnRefreshButtonClicked);
+    }
+
+    private void OnRefreshButtonClicked()
+    {
+        foreach (Transform child in _RoomListContent)
+            Destroy(child.gameObject);
+
+        CachedRoomList.SetRoomList(new List<RoomInfo>());
+        _SelectedRoomInfo = null;
+        _JoinRoomBtn.interactable = false;
+
+        PhotonNetwork.LeaveLobby();
+    }
+
+    public override void OnLeftLobby()
+    {
+        PhotonNetwork.JoinLobby();
     }
 
     private void HandleSelectRoom(RoomInfo info)
     {
-        _SelectedRoomInfo = info;
-        _JoinRoomButton.interactable = true;
+        _SelectedRoomInfo         = info;
+        _JoinRoomBtn.interactable = true;
     }
 
     private void HandleUpdateRoomList(List<RoomInfo> roomList)
     {
         CachedRoomList.SetRoomList(roomList);
 
-        ClearRoomListUI();
+        foreach (Transform child in _RoomListContent)
+            Destroy(child.gameObject);
 
         foreach (var info in roomList)
         {
             if (info.RemovedFromList) continue;
-
-            var item = Instantiate(_RoomItemPrefab, _RoomListContent);
-
+            var item    = Instantiate(_RoomItemPrefab, _RoomListContent);
             var manager = item.GetComponent<RoomItemUIManager>();
             manager.SetInfo(info);
-        }
-    }
-
-    private void ClearRoomListUI()
-    {
-        foreach (Transform child in _RoomListContent)
-        {
-            Destroy(child.gameObject);
         }
     }
 
@@ -83,8 +98,6 @@ public class LobbyUIManager : MonoBehaviour
     private void OnJoinRoomButtonClicked()
     {
         if (!string.IsNullOrEmpty(_SelectedRoomInfo.Name))
-        {
             GameEvents.RaiseRequestJoinRoom(_SelectedRoomInfo);
-        }
     }
 }
