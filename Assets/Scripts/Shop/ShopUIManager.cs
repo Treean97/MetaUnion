14e31b2875070a78be9b591f2d48c8c2ac0dc9aa
Weminsoo <weminsoo@naver.com>
2025-08-11 21:59:54 +0900

    판매 기능 추가 작업

--- Assets/Scripts/Shop/ShopUIManager.cs
+++ Assets/Scripts/Shop/ShopUIManager.cs
@@ -4,18 +4,26 @@ using UnityEngine.UI;
 
 public class ShopUIManager : MonoBehaviour
 {
-    [SerializeField] private CustomizeItemPoolSO _ItemPool;
-    [SerializeField] private Transform _Contents;
-    [SerializeField] private GameObject _SlotPrefab;
+    [Header("Buy Area")]
+    [SerializeField] private CustomizeItemPoolSO _BuyItemPool;
+    [SerializeField] private Transform _BuyContents;
+    [SerializeField] private GameObject _BuySlotPrefab;
     [SerializeField] private List<Button> _CategoryBtns;
-    [SerializeField] private Button _CloseBtn;
 
     private ItemType _CurType;
+
+    [Header("Sell Area")]
+    [SerializeField] private ItemDataPoolSO _SellItemPool;
+    [SerializeField] private Transform _SellContents;
+    [SerializeField] private GameObject _SellSlotPrefab;
+    [SerializeField] private Button _CloseBtn;
+
+
 
     void Awake()
     {
         // 초기 카테고리와 비활성화
-        _CurType = _ItemPool.GetFirstType();
+        _CurType = _BuyItemPool.GetFirstType();
 
         // 카테고리 버튼 세팅
         for (int i = 0; i < _CategoryBtns.Count; i++)
@@ -32,7 +40,7 @@ public class ShopUIManager : MonoBehaviour
         // UI 켜짐(입력제한)
         InputBlock.BlockInput();
         // 잠긴 아이템 리스트 요청
-        GameEvents.OnProvideLockedItems += HandleProvideItems;
+        GameEvents.OnProvideLockedItems += HandleProvideBuyItems;
         // 활성화될 때 현재 카테고리 요청
         GameEvents.RaiseRequestLockedItems(_CurType);
         // 상점 구매 시 카테고리 갱신을 위한 이벤트
@@ -42,7 +50,7 @@ public class ShopUIManager : MonoBehaviour
     void OnDisable()
     {
         InputBlock.UnblockInput();
-        GameEvents.OnProvideLockedItems -= HandleProvideItems;
+        GameEvents.OnProvideLockedItems -= HandleProvideBuyItems;
         GameEvents.OnItemPurchaseSuccess -= HandlePurchaseSueecss;
     }
 
@@ -58,16 +66,16 @@ public class ShopUIManager : MonoBehaviour
         GameEvents.RaiseRequestLockedItems(_CurType);
     }
 
-    void HandleProvideItems(List<CustomizeItemSO> items)
+    void HandleProvideBuyItems(List<CustomizeItemSO> items)
     {
         // 기존 슬롯 제거
-        foreach (Transform c in _Contents)
+        foreach (Transform c in _BuyContents)
             Destroy(c.gameObject);
 
         foreach (var item in items)
         {
             // 1) 프리팹 인스턴스 생성
-            var go = Instantiate(_SlotPrefab, _Contents);
+            var go = Instantiate(_BuySlotPrefab, _BuyContents);
 
             // 2) ShopItemSlot 컴포넌트 꺼내기
             var slot = go.GetComponent<ShopItemSlot>();
@@ -80,6 +88,11 @@ public class ShopUIManager : MonoBehaviour
             // 3) 슬롯 세팅 (이 안에서 버튼 클릭 리스너까지 모두 처리됨)
             slot.Setup(item);
         }
+    }
+
+    void HandleProvideSellItems(List<ItemDataSO> items)    
+    {
+
     }
 
 
