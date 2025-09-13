0422631761bd8f2835193d9458e2d71bf9e4514d
Weminsoo <weminsoo@naver.com>
2025-09-13 21:23:36 +0900

    UI 리팩토링

--- Assets/Scripts/GameManager/GraphicManager.cs
+++ Assets/Scripts/GameManager/GraphicManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
-public class SettingManager : MonoBehaviour
+public class GraphicManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Application.targetFrameRate = 144;
 
+        Screen.SetResolution(1920, 1080, false); // false면 창모드, true면 전체화면
     }
 
     // Update is called once per frame
