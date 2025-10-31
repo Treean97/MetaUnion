e911b0facca80dc3bc6f6471ed04078008ca0ff6
Weminsoo <weminsoo@naver.com>
2025-10-31 19:30:56 +0900

    점프 애니메이션 변경

--- Assets/Hanzzz/MeshDemolisher/Examples/Scripts/MeshDemolisherExample.cs
+++ Assets/Hanzzz/MeshDemolisher/Examples/Scripts/MeshDemolisherExample.cs
@@ -8,38 +8,48 @@ namespace Hanzzz.MeshDemolisher
     {
         [Header("Hint: right click on the script to call Demolish and Reset\nin the editor mode.")]
         [Space]
-        [SerializeField] private GameObject targetGameObject;
-        [SerializeField] private Transform breakPointsParent;
-        [SerializeField] private Material interiorMaterial;
+        [SerializeField] private GameObject _TargetGameObject;
+        [SerializeField] private Transform _BreakPointsParent;
+        [SerializeField] private Material _InteriorMaterial;
 
-        [SerializeField] [Range(0f,1f)] private float resultScale;
-        [SerializeField] private Transform resultParent;
+        [SerializeField] [Range(0f,1f)] private float _ResultScale;
+        [SerializeField] private Transform _ResultParent;
+
+        [Header("Break Power")]
+        [SerializeField] float _Mass = 0.25f;
+        [SerializeField] float _EjectForceMin = 2.0f;
+        [SerializeField] float _EjectForceMax = 3.0f;
+        [SerializeField] float _EjectTorque   = 1.5f;
         private IDestructible _Destrutible;
 
 
         // [SerializeField] private TMP_Text logText;
 
-        private static MeshDemolisher meshDemolisher = new MeshDemolisher();
+        private static MeshDemolisher _MeshDemolisher = new MeshDemolisher();
 
-        void Start()
+        void Awake()
         {
-            _Destrutible = targetGameObject.GetComponentInParent<IDestructible>();
-            _Destrutible.OnDestroyed += Demolish;
+            _Destrutible = GetComponentInParent<IDestructible>();
         }
 
-        void OnDestroyed()
+        void OnEnable()
         {
-            _Destrutible.OnDestroyed -= Demolish;
+            if (_Destrutible != null) _Destrutible.OnDestroyed += Demolish;
+        }
+
+        void OnDisable()
+        {
+            if (_Destrutible != null) _Destrutible.OnDestroyed -= Demolish;
         }
 
         [ContextMenu("Verify Demolish Input")]
         public void VerifyDemolishInput()
         {
-            List<Transform> breakPoints = Enumerable.Range(0,breakPointsParent.childCount).Select(x=>breakPointsParent.GetChild(x)).ToList();
+            List<Transform> breakPoints = Enumerable.Range(0,_BreakPointsParent.childCount).Select(x=>_BreakPointsParent.GetChild(x)).ToList();
 
             // Passing this verification does not mean the input is valid.
             // Refer to the documentation to see all input requirements.
-            bool res = meshDemolisher.VerifyDemolishInput(targetGameObject, breakPoints);
+            bool res = _MeshDemolisher.VerifyDemolishInput(_TargetGameObject, breakPoints);
             if(res)
             {
                 Debug.Log("Demolish input looks good.");
@@ -49,59 +59,54 @@ namespace Hanzzz.MeshDemolisher
         [ContextMenu("Demolish")]
         public void Demolish()
         {
-            Enumerable.Range(0,resultParent.childCount).Select(i=>resultParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
-            List<Transform> breakPoints = Enumerable.Range(0,breakPointsParent.childCount).Select(x=>breakPointsParent.GetChild(x)).ToList();
+            Enumerable.Range(0,_ResultParent.childCount).Select(i=>_ResultParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
+            List<Transform> breakPoints = Enumerable.Range(0,_BreakPointsParent.childCount).Select(x=>_BreakPointsParent.GetChild(x)).ToList();
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            List<GameObject> res = meshDemolisher.Demolish(targetGameObject, breakPoints, interiorMaterial);
+            List<GameObject> res = _MeshDemolisher.Demolish(_TargetGameObject, breakPoints, _InteriorMaterial);
             watch.Stop();
             // logText.text = $"Demolish time: {watch.ElapsedMilliseconds}ms.";
 
-            res.ForEach(x=>x.transform.SetParent(resultParent, true));
-            Enumerable.Range(0,resultParent.childCount).Select(i=>resultParent.GetChild(i)).ToList().ForEach(x=>x.localScale=resultScale*Vector3.one);
-            AddSimplePhysics(res, targetGameObject.transform.position);
-            targetGameObject.SetActive(false);
+            res.ForEach(x=>x.transform.SetParent(_ResultParent, true));
+            Enumerable.Range(0,_ResultParent.childCount).Select(i=>_ResultParent.GetChild(i)).ToList().ForEach(x=>x.localScale=_ResultScale*Vector3.one);
+            AddSimplePhysics(res, _TargetGameObject.transform.position);
+            _TargetGameObject.SetActive(false);
         }
 
         [ContextMenu("Demolish Async")]
         public async void DemolishAsync()
         {
-            Enumerable.Range(0,resultParent.childCount).Select(i=>resultParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
-            List<Transform> breakPoints = Enumerable.Range(0,breakPointsParent.childCount).Select(x=>breakPointsParent.GetChild(x)).ToList();
+            Enumerable.Range(0,_ResultParent.childCount).Select(i=>_ResultParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
+            List<Transform> breakPoints = Enumerable.Range(0,_BreakPointsParent.childCount).Select(x=>_BreakPointsParent.GetChild(x)).ToList();
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            List<GameObject> res = await meshDemolisher.DemolishAsync(targetGameObject, breakPoints, interiorMaterial);
+            List<GameObject> res = await _MeshDemolisher.DemolishAsync(_TargetGameObject, breakPoints, _InteriorMaterial);
             watch.Stop();
             // logText.text = $"Demolish time: {watch.ElapsedMilliseconds}ms.";
 
-            res.ForEach(x=>x.transform.SetParent(resultParent, true));
-            Enumerable.Range(0,resultParent.childCount).Select(i=>resultParent.GetChild(i)).ToList().ForEach(x=>x.localScale=resultScale*Vector3.one);
+            res.ForEach(x=>x.transform.SetParent(_ResultParent, true));
+            Enumerable.Range(0,_ResultParent.childCount).Select(i=>_ResultParent.GetChild(i)).ToList().ForEach(x=>x.localScale=_ResultScale*Vector3.one);
 
-            targetGameObject.SetActive(false);
+            _TargetGameObject.SetActive(false);
         }
 
         [ContextMenu("Reset")]
         public void Reset()
         {
             //Enumerable.Range(0,breakPointsParent.childCount).Select(i=>breakPointsParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
-            Enumerable.Range(0,resultParent.childCount).Select(i=>resultParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
+            Enumerable.Range(0,_ResultParent.childCount).Select(i=>_ResultParent.GetChild(i)).ToList().ForEach(x=>DestroyImmediate(x.gameObject));
 
-            targetGameObject.SetActive(true);
+            _TargetGameObject.SetActive(true);
         }
 
         public void OnValidate()
         {
-            Enumerable.Range(0, resultParent.childCount).Select(i => resultParent.GetChild(i)).ToList().ForEach(x => x.localScale = resultScale * Vector3.one);
+            Enumerable.Range(0, _ResultParent.childCount).Select(i => _ResultParent.GetChild(i)).ToList().ForEach(x => x.localScale = _ResultScale * Vector3.one);
         }
         
         void AddSimplePhysics(List<GameObject> pieces, Vector3 center)
         {
             if (pieces == null) return;
-
-            const float mass = 0.25f;
-            const float fMin = 3.0f;
-            const float fMax = 6.0f;
-            const float torque = 2.0f;
 
             float lifeSec = Mathf.Max(0.1f, RespawnManager.GlobalBreakFxSeconds);
 
@@ -123,7 +128,7 @@ namespace Hanzzz.MeshDemolisher
                     }
                 }
 
-                rb.mass = mass;
+                rb.mass = _EjectForceMin;
                 rb.interpolation = RigidbodyInterpolation.Interpolate;
                 rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
 
@@ -137,8 +142,8 @@ namespace Hanzzz.MeshDemolisher
                 var dir = (go.transform.position - center).normalized;
                 if (dir.sqrMagnitude < 1e-4f) dir = Random.onUnitSphere;
 
-                rb.AddForce(dir * Random.Range(fMin, fMax), ForceMode.Impulse);
-                rb.AddTorque(Random.insideUnitSphere * torque, ForceMode.Impulse);
+                rb.AddForce(dir * Random.Range(_EjectForceMin, _EjectForceMax), ForceMode.Impulse);
+                rb.AddTorque(Random.insideUnitSphere * _EjectTorque, ForceMode.Impulse);
 
                 // 수명 정리
                 if (lifeSec > 0f) Destroy(go, lifeSec);
